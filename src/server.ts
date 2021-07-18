24e48ffbc6ffa7632d790a0011b22a0a3c2bd424
rdrgzma <rdrgzma@hotmail.com>
2021-07-18 02:07:43 -0300

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,38 @@
-import express from 'express';
+import express, { json } from 'express';
 import dotenv from 'dotenv';
-//import path from 'path';
-//import mainRoutes from './routes';
 
 dotenv.config();
 const server = express();
+server.use(express.json());
 
-
+// Listar
 server.get("/",(req,res)=>{
-    res.send("Servidor funcionando"); 
+    return res.json({"mensagem": "Funcionando"}); 
+});
+
+//Criar
+server.post("/",(req,res)=>{
+    let{nome , email} = req.body;
+    return res.json({nome,email});
+});
+
+//Atualizar
+server.put("/:id", (req,res)=>{
+    const {id} = req.params;
+    const {nome,email} = req.body;
+    return res.json({
+        id,
+        nome,
+        email
+    });
+})
+
+//Apagar
+server.delete("/:id", (req,res)=>{
+    const {id} = req.params;
+    return res.json({
+        id
+    })
 });
 
 
