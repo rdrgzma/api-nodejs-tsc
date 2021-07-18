4c91a6e730c01b67a7b29f89572928b5604e9928
rdrgzma <rdrgzma@hotmail.com>
2021-07-18 00:29:31 -0300

    fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
+//import path from 'path';
 //import mainRoutes from './routes';
 
 dotenv.config();
 const server = express();
 
 
-server.use((req,res)=>{
-    res.render('pages/404');
+server.get("/",(req,res)=>{
+    res.send("Servidor funcionando"); 
 });
 
 
-server.listen(process.env.PORT);
+server.listen(process.env.PORT, ()=>{console.log(`Servidor rodando na porta: ${process.env.PORT}`)});
